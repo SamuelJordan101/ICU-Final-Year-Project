ea36030c72b18df0ab4435d238fcb138ef1b7fe3
SamuelJordan101 <44780790+SamuelJordan101@users.noreply.github.com>
2021-01-18 20:18:39 +0000

    Storyboard Feedback & Started Prototype

---
+++ Tracker App/Tracker App/Tracker App/Tracker App/Login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tracker_App
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        void Login_Button(object sender, System.EventArgs e)
+        {
+            var ID = EnterID.Text;
+
+            if (ID != null && ValidateAmount() == true)
+            {
+                Navigation.PushModalAsync(new Tracker_App.Home());
+            } else {
+                DisplayAlert("Invalid Entry!", "Please Enter a valid ID", "OK");
+                EnterID.Text = "";
+            }
+        }
+
+        private bool ValidateAmount()
+        {
+            if (int.TryParse(EnterID.Text, out int result))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
